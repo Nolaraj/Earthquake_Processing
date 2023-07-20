84d65c6186f6bfe66755339a595d9b9f34d5a07a
Nolaraj Poudel <pnolaraj@gmail.com>
2023-07-20 08:47:53 +0545

    Create Utilities.py

---
+++ Utilities.py
@@ -0,0 +1,18 @@
+def Data_Refinery(rec):
+    import re
+    def extract_numbers(string):
+        # pattern = r'-?\d+(?:\.\d+)?E-?\d+'
+
+        pattern = r'-?\d+(?:\.\d+)?(?:E-?\d+)?'  # Regular expression pattern to match decimal numbers and scientific notation
+        numbers = re.findall(pattern, string)
+        return numbers
+
+    for index, items in enumerate(rec):
+        if index == 0:
+            dataset = extract_numbers(items)
+        else:
+            dataset += extract_numbers(items)
+
+    dataset = [float(x) for x in dataset]
+    dataset = np.array(dataset).astype("float")
+    return dataset
